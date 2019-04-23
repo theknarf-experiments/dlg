0c404027e3269401b6bb0d0d45c7c8e2ef92422e
Frank Lyder Bredland <fl@nyggen.com>
2019-04-23 16:54:35 +0200

    Setup commander for bin

--- bin/dlg.js
+++ bin/dlg.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
 
-console.log(`Welcome to the dlg repl\n`);
+var program = require('commander');
 
-require('../src/repl.js');
+program
+	.version('0.0.1', '-v, --version')
+	.usage('[options] <files ...>');
 
+program
+	.command('init')
+	.description('Initialize a new repo for use with dlg')
+	.option('-b, --bare', 'Work with bare repo')
+	.action((env, options) => {
+		console.err('Not implemented yet');
+	});
+
+program
+	.command('repl')
+	.description('Run the repl')
+	.action((env, options) => {
+		console.log(`Welcome to the dlg repl\n`);
+		require('../src/repl.js');
+	});
+
+program.parse(process.argv);
+
+const NO_COMMAND_SPECIFIED = program.args.length === 0;
+if (NO_COMMAND_SPECIFIED) {
+  program.help();
+}
